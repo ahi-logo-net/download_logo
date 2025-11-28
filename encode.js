0375b8dcf1c2f3699d24a101af83ba7613a26b05
ahi-logo-net <sr0210046@gmail.com>
2025-11-28 11:00:41 +0900

    Update encode.js

--- encode.js
+++ encode.js
@@ -6,6 +6,7 @@ const type = Number(params.get('a')) || 0;
 const textColor = params.get('b') || '#fff';
 const typeBefore = ['空想','',''];
 const typeAfter = ['','都ふぁん','くらぶ'];
+const referrer = document.referrer();
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
